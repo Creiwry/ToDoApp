c6338fc5c0dd00f892fe1a93d54c7f69f865ba72
Creiwry <jay.gardos@outlook.com>
2024-01-09 00:22:32 +0100

    feat: add basic scoring functionality

---
+++ assets/images/index.js
@@ -0,0 +1,9 @@
+export const FLOWERCENTER = require('./flower-purple-center.png');
+export const FLOWER1 = require('./flower-purple1.png');
+export const FLOWER2 = require('./flower-purple2.png');
+export const FLOWER3 = require('./flower-purple3.png');
+export const FLOWER4 = require('./flower-purple4.png');
+export const FLOWER5 = require('./flower-purple5.png');
+export const FLOWER6 = require('./flower-purple6.png');
+export const FLOWER7 = require('./flower-purple7.png');
+export const FLOWER8 = require('./flower-purple8.png');
--- screens/ToDoList.jsx
+++ screens/ToDoList.jsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
+import { FLOWER1, FLOWER2, FLOWER3, FLOWER4, FLOWER5, FLOWER6, FLOWER7, FLOWER8, FLOWERCENTER } from '../assets/images';
 import { useState, useEffect } from 'react';
 import { useFonts, Montserrat_800ExtraBold } from '@expo-google-fonts/montserrat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Pressable, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity, Pressable, KeyboardAvoidingView } from 'react-native';
 
+const flowerImages = [
+  FLOWERCENTER,
+  FLOWER1,
+  FLOWER2,
+  FLOWER3,
+  FLOWER4,
+  FLOWER5,
+  FLOWER6,
+  FLOWER7,
+  FLOWER8,
+];
 
 const ToDoList = () => {
   const [task, setTask] = useState({text: '', done: false});
   const [tasks, setTasks] = useState([]);
+  const [lastDate, setLastdate] = useState(null);
+  const [score, setScore] = useState(0);
+  const [flowerPaths, setFlowerPaths] = useState([]);
 
   let [fontsLoaded] = useFonts({
     Montserrat_800ExtraBold,
@@ -24,9 +39,64 @@ const ToDoList = () => {
     }
   };
 
-  const fetchTasks = async () => {
+  const saveOldData = async (oldScore, oldDate) => {
     try {
+      const pastScores = await AsyncStorage.getItem('pastScores')
+      let newPastScores =[]
+      if (pastScores){
+        newPastScores = [...JSON.parse(pastScores), {date: oldDate, score: oldScore}]
+      } else {
+        newPastScores = [{date: oldDate, score: oldScore}]
+      }
+      await AsyncStorage.setItem('pastScores', JSON.stringify(newPastScores))
+      await AsyncStorage.setItem('score', '0')
+      const currentDate = new Date().toLocaleDateString();
+      await AsyncStorage.setItem('date', currentDate)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const flowerPathLogic = (score) => {
+    const completeFlowers = Math.floor(score / 8);
+    const remainder = score % (flowerImages.length - 1);
+
+    let flowerRoutes = [];
+
+    for (let i = 0; i < completeFlowers; i++) {
+      flowerRoutes.push(8);
+    }
+
+    if (remainder !== 0) {
+      flowerRoutes.push(remainder);
+    }
+
+    if (flowerRoutes.length === 0) {
+      return [0];
+    }
+    console.log(flowerRoutes)
+    return flowerRoutes;
+  }
+
+  const loadData = async () => {
+    try {
+      let storedDate = await AsyncStorage.getItem('date')
+      const currentDate = new Date().toLocaleDateString();
+
+      if (currentDate !== storedDate) {
+        saveOldData(storedScore, storedDate)
+      }
+
+      const storedScore = await AsyncStorage.getItem('score')
+      storedDate = await AsyncStorage.getItem('date')
+
+      if (storedScore && storedDate) {
+        setScore(parseInt(storedScore, 10));
+        setLastdate(storedDate);
+      }
+
       const fetchedTasks = await AsyncStorage.getItem('tasks');
+
       if (fetchedTasks) {
         setTasks(JSON.parse(fetchedTasks));
       }
@@ -37,7 +107,9 @@ const ToDoList = () => {
 
   useEffect(() => {
     const loadFonts = async () => {
-      await fetchTasks();
+      await AsyncStorage.setItem('score', score.toString());
+      await loadData();
+      setFlowerPaths(flowerPathLogic(score))
     }
     loadFonts();
   }, []);
@@ -64,7 +136,21 @@ const ToDoList = () => {
   const toggleDone = async (index) => {
     try {
       const updatedTasks = [...tasks]
-      updatedTasks[index].done = !updatedTasks[index].done
+      if (!updatedTasks[index].done){
+        updatedTasks[index].done = !updatedTasks[index].done
+        setScore((prevScore) => {
+          const newScore = prevScore + 1
+          setFlowerPaths(flowerPathLogic(newScore))
+          return newScore
+        })
+      } else {
+        updatedTasks[index].done = !updatedTasks[index].done
+        setScore((prevScore) => {
+          const newScore = prevScore - 1
+          setFlowerPaths(flowerPathLogic(newScore))
+          return newScore
+        })
+      }
       await AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks));
       setTasks(updatedTasks);
     } catch (error) {
@@ -76,15 +162,30 @@ const ToDoList = () => {
     <View 
       style={styles.screen}
     >
-    <View style={{flex: 4}}>
-        <TextInput 
-          value={task.text}
-          onChangeText={(text) => setTask({text: text, done: false})}
-          onSubmitEditing={saveTask}
-          style={styles.textInput}
-          placeholderTextColor='white'
-          placeholder='Type here to add task!'>
-        </TextInput>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <Pressable 
+          style={styles.button}
+          onPress={handleClear}>
+          <Text
+          style={styles.buttonText}>
+            Clear Tasks
+          </Text>
+          </Pressable>
+        <Pressable 
+          style={styles.button}
+          onPress={()=>setScore(0)}>
+          <Text
+          style={styles.buttonText}>
+            Clear Score 
+          </Text>
+          </Pressable>
+      </View>
+    <View style={{flex: 5}}>
+      <View style={styles.imageContainer}>
+        {flowerPaths.map((number, index) => (
+            <Image key={index} source={flowerImages[number]} style={styles.image}/>
+        ))}
+      </View>
       {tasks.map((t, index) => (
         <View key={index} style={styles.taskContainer}>
           <TouchableOpacity
@@ -110,16 +211,14 @@ const ToDoList = () => {
         </View>
       ))}
         </View>
-      <View style={{flex: 1}}>
-        <Pressable 
-          style={styles.button}
-          onPress={handleClear}>
-          <Text
-          style={styles.buttonText}>
-            Clear Completed Tasks
-          </Text>
-          </Pressable>
-      </View>
+        <TextInput 
+          value={task.text}
+          onChangeText={(text) => setTask({text: text, done: false})}
+          onSubmitEditing={saveTask}
+          style={styles.textInput}
+          placeholderTextColor='white'
+          placeholder='Type here to add task!'>
+        </TextInput>
     </View>
   )
 }
@@ -130,6 +229,15 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     padding: 10,
     flexDirection: 'column',
+  },
+    imageContainer: {
+    flexDirection: 'row',
+  },
+    image: {
+    width: 100,
+    height: 100,
+    margin: 10,
+    resizeMode: 'cover',
   },
   textInput: {
     backgroundColor: "black",
